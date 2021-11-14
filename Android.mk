42ae99ebf5a155ba627aae01253bd14082a5eae3
RaviJavaDeveloper <sukolaravi@gmail.com>
2021-11-14 04:44:18 +0000

    build fix

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter r8q$(TARGET_DEVICE)),)
+ifneq ($(filter r8q,$(TARGET_DEVICE)),)
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,113 +84,137 @@ DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/samsung_manifest.xml
 # vendor compatibility matrix
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := kona
-TARGET_NO_BOOTLOADER := true
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/r8q
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CLANG_COMPILE := true
-
-# Image
-BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
-BOARD_KERNEL_SECOND_OFFSET := 0x00F00000
-BOARD_KERNEL_SEPARATED_DTBO := true
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_RAMDISK_OFFSET := 0x02000000
-BOARD_DTB_OFFSET := 0x01F00000
-BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --dtb_offset $(BOARD_DTB_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
+TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/samsung/r8q
+TARGET_KERNEL_CONFIG := r8q_defconfig
 # Partitions
-BOARD_FLASH_BLOCK_SIZE := 262144
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 82694144
+BOARD_CACHEIMAGE_PARTITION_SIZE := 629145600
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
-BOARD_CACHEIMAGE_PARTITION_SIZE := 2097152
+BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 82694144
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 118154457088
-BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 536870912
 
-# Dynamic Partitions -- system only
-BOARD_SUPER_PARTITION_SIZE := 8320450560
-BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
-BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 8320446464
-BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := \
-    system
+### CACHE
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 
-# File System
-TARGET_COPY_OUT_VENDOR := vendor
+### Metadata
+BOARD_USES_METADATA_PARTITION := true
+
+### ODM
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_ODM := odm
-TARGET_COPY_OUT_PRODUCT := system/product
-TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
+
+### PRODUCT
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_PRODUCT := product
+
+### SYSTEM
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+
+### SYSTEM_EXT
+TARGET_COPY_OUT_SYSTEM_EXT := system/system_ext
+
+### USERDATA
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_ROOT_EXTRA_FOLDERS := \
-    prism \
-    optics \
-    metadata \
-    efs \
-    carrier \
-    dqmdbg \
-    spu \
-    keydata \
-    keyrefuge
 
-# Android Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_ROLLBACK_INDEX := 0
-# HASHTREE_DISABLED | VERIFICATION_DISABLED
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+### VENDOR
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
 
-# Recovery
-BOARD_HAS_DOWNLOAD_MODE := true
-BOARD_INCLUDE_RECOVERY_DTBO := true
-TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
-TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+### Dynamic Partitions
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product system vendor
+BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
 
-# APEX
-DEXPREOPT_GENERATE_APEX_IMAGE := true
+ifneq ($(WITH_GMS),true)
+BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := -1
+endif
+BOARD_ODMIMAGE_PARTITION_RESERVED_SIZE := 30720000
+BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 30720000
 
-# Audio
-USE_XML_AUDIO_POLICY_CONF := 1
-
-# Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := kona
 
 # Properties
-BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+
+# Recovery
+BOARD_INCLUDE_RECOVERY_DTBO := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 
-# SELinux
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public \
-    device/qcom/sepolicy/qva/public \
-    device/samsung_slsi/sepolicy/common/public \
-    $(COMMON_PATH)/sepolicy/platform/public
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+TARGET_USES_VND_SECRIL := true
+BOARD_PROVIDES_LIBRIL := true
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private \
-    device/samsung_slsi/sepolicy/common/private \
-    $(COMMON_PATH)/sepolicy/platform/private
+# Root
+BOARD_ROOT_EXTRA_FOLDERS := \
+    cache \
+    carrier \
+    efs \
+    keydata \
+    keyrefuge \
+    metadata \
+    optics \
+    prism \
+    spu \
+    omr
 
-PRODUCT_PUBLIC_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/product/public \
-    $(COMMON_PATH)/sepolicy/public
+# Samsung
+BOARD_VENDOR := samsung
 
-PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/product/private \
-    $(COMMON_PATH)/sepolicy/private
+# Security patch level
+VENDOR_SECURITY_PATCH := 2021-09-01
 
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+# Sepolicy
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
+# WLAN
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_DEFAULT := qca_cld3
+WIFI_DRIVER_STATE_CTRL_PARAM := "/dev/wlan"
+WIFI_DRIVER_STATE_OFF := "OFF"
+WIFI_DRIVER_STATE_ON := "ON"
+WIFI_RESET_IFACE_AFTER_MAC_CHANGE := true
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# Inherit from proprietary vendor
+-include vendor/samsung/sm8250-common/BoardConfigVendor.mk
